8c136b57b9189bf7cdef621fe812e6b21dd98731
Kristina0063 <ivankovickristina98@gmail.com>
2025-06-18 14:48:35 +0200

    Napravljeni su modeli, neophodno je popuniti logiku svakog modela u skladu sa pravilima iz konceptualnog modela

---
+++ app/Models/Sugestija.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Sugestija extends Model
+{
+    /** @use HasFactory<\Database\Factories\SugestijaFactory> */
+    use HasFactory;
+}
